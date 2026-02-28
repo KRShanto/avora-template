e9bb339a406158ecd2c4bb51ae00ae0068277704
KR Shanto <krshanto2005@gmail.com>
2026-02-28 22:41:15 +0600

    config: Configure Next.js to ignore TypeScript build errors during the build process.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true, // This ignores ts errors during build
+  },
 };
 
 module.exports = nextConfig;
