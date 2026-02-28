2890b252298828853f5ce5f1bcb56cdcb29b7da8
KR Shanto <krshanto2005@gmail.com>
2026-02-28 15:24:48 +0600

    chore(home): remove unnecessary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import TopNavOne from "@/components/Header/TopNav/TopNavOne";
-import WhatNewOne from "@/components/Home1/WhatNewOne";
 import productData from "@/data/Product.json";
-import Collection from "@/components/Home1/Collection";
-import TabFeatures from "@/components/Home1/TabFeatures";
-import Banner from "@/components/Home1/Banner";
 import Benefit from "@/components/Home1/Benefit";
-import testimonialData from "@/data/Testimonial.json";
-import Testimonial from "@/components/Home1/Testimonial";
-import Instagram from "@/components/Home1/Instagram";
-import Brand from "@/components/Home1/Brand";
 import Footer from "@/components/Footer/Footer";
 import MenuEight from "@/components/Header/Menu/MenuEight";
 import SliderTen from "@/components/Slider/SliderTen";
@@ -33,13 +25,7 @@ export default function Home() {
       </div>
       <TrendingNow />
       <TrendingProduct data={productData} start={12} limit={20} />
-      <Collection />
-      <TabFeatures data={productData} start={0} limit={6} />
-      <Banner />
       <Benefit props="md:py-20 py-10" />
-      <Testimonial data={testimonialData} limit={6} />
-      <Instagram />
-      <Brand />
       <Footer />
     </>
   );
